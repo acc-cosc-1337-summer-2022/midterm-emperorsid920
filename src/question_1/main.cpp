7b4ef7b6d5699b3f621201126de5c28d6d94bb7b
emperorsid920 <66718954+emperorsid920@users.noreply.github.com>
2022-06-30 21:01:27 -0500

    Update main.cpp

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,24 @@
-
+#include <iostream>
+#include <iomanip>
+#include ""
+using namespace std;
 int main()
 {
+ 
+    do {
+             
+          
+            
+            
 
+            
+             cout<<"Are you done? " <<"Y or N: ";
+             cin>>ans;
+             
+             if (ans == 'y' || ans == 'Y') {
+                 done = true;
+             }
+         }
+         while (!done);
     return 0;
 }
