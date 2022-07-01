267ee9571fff7cd12a63edf9b865269adc5c2a83
emperorsid920 <66718954+emperorsid920@users.noreply.github.com>
2022-06-30 20:58:18 -0500

    Update question1.cpp

--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -5,4 +5,13 @@ bool test_config()
     return true;
 }
 
-
+double get_fahrenheit(double c)
+{
+    double FHeit;
+    
+    const double X = double(9)/double(5);
+    
+    FHeit = X*c + 32;
+    
+    return FHeit;
+}
