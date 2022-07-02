33ef9bdfe4332742e20a5a0f5a29f6a4a8f61818
emperorsid920 <66718954+emperorsid920@users.noreply.github.com>
2022-07-02 17:35:31 -0500

    Update main.cpp

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,58 @@
 
-int main()
-{
 
+#include <iostream>
+#include "question2.h"
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    double Meal_Price;
+    double Grat_Rate;
+    double Amount_Tax;
+    double Amount_Grat;
+    double Final_Amount;
+
+    
+    bool done = false;
+    char ans = ' ';
+    
+    while (!done) {
+        cout << "Please Enter Meal Price: ";
+        cin>>Meal_Price;
+        
+        cout << "Please Rate of Gratuity: ";
+        cin>>Grat_Rate;
+        
+        
+        Reciept Rec(Meal_Price,Grat_Rate);
+        
+        Amount_Tax = Rec.calculate_tax();
+        
+        Amount_Grat = Rec.calculate_gratuity();
+        
+        Final_Amount = Meal_Price + Amount_Tax + Amount_Grat;
+        
+        Rec.display_receipt(Final_Amount);
+        
+        cout<<"Do you want to continue? Y or N";
+        cin>>ans;
+        
+        if (ans == 'N' || ans == 'n') {
+            done = true;
+        }
+        
+    }
+    
+   
+    
+    //Testers
+    //cout<<Amount_Tax<<endl;
+    //cout<<Amount_Grat<<endl;
+    
+   
+    
+    
+    
     return 0;
 }
