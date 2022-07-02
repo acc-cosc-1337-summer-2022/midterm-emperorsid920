e6670f34deea4c81e7b76c890150fcd7577ae254
emperorsid920 <66718954+emperorsid920@users.noreply.github.com>
2022-07-02 13:23:17 -0500

    Update question2.h

--- src/question_2/question2.h
+++ src/question_2/question2.h
@@ -1,1 +1,16 @@
 bool test_config();
+
+class Reciept {       // The class
+   private:
+    // Private attribute
+    double Gratuity;
+    const double Tax = 0.08;
+    double Price;
+  
+  public:             // Access specifier
+    Reciept(double, double); 
+    double calculate_tax(double);
+    double  calculate_gratuity(double);
+    void display_receipt(double);
+  
+};
